7a7c5eafb938092620b1da5c50ed4d8648a009f7
mzyzc <mzyzc@czyz.xyz>
2022-01-23 16:47:49 +0000

    Removed unnecessary newlines and reformatted some code

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,27 @@ use matrix_sdk::{
     },
 };
 
-async fn login(username: String, password: String, homeserver: String) -> Result<matrix_sdk::Client> {
+#[tokio::main]
+async fn main() -> Result<()> {
+    let username = env::var("USER").unwrap();
+    let password = env::var("PASS").unwrap();
+    let homeserver = env::var("SRV").unwrap();
 
+    let client = login(username, password, homeserver).await.unwrap();
+    client.sync_once(SyncSettings::default()).await?;
+    setup(&client).await;
+
+    client.register_event_handler(|ev : SyncMessageEvent<MessageEventContent>| async move {
+        on_room_message(ev).await;
+    }).await;
+
+    println!("Syncing");
+    client.sync(SyncSettings::default()).await;
+
+    Ok(())
+}
+
+async fn login(username: String, password: String, homeserver: String) -> Result<matrix_sdk::Client> {
     let homeserver_url = Url::parse(&homeserver)
         .expect("Couldn't parse the homeserver URL");
     let client = Client::new(homeserver_url)
@@ -34,26 +53,4 @@ async fn setup(client: &matrix_sdk::Client) {
 
 async fn on_room_message(event: SyncMessageEvent<MessageEventContent>) {
     print!("{:?}", event);
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-
-    let username = env::var("USER").unwrap();
-    let password = env::var("PASS").unwrap();
-    let homeserver = env::var("SRV").unwrap();
-
-    let client = login(username, password, homeserver).await.unwrap();
-
-    client.sync_once(SyncSettings::default()).await?;
-
-    setup(&client).await;
-
-    client.register_event_handler(|ev : SyncMessageEvent<MessageEventContent>| async move {on_room_message(ev).await;}).await;
-
-    println!("Syncing");
-
-    client.sync(SyncSettings::default()).await;
-
-    Ok(())
 }
