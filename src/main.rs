efe0cddf13bcf47235d21d8983692c73d1a8173f
mzyzc <mzyzc@czyz.xyz>
2022-01-23 17:18:21 +0000

    Added dotenv for setup convenience

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 pub mod bot;
 use std::env;
+use dotenv;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
     let username = env::var("USER").unwrap();
     let password = env::var("PASS").unwrap();
     let homeserver = env::var("SRV").unwrap();
